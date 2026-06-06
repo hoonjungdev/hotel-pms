40fc6126ee738b89a379a0fe980dd5243ae7803a
Hoon Jung <hoonjungdev@gmail.com>
2026-06-06 22:31:56 +0900

    refactor: move workspace tenant to shared scope

--- src/HotelPms/Features/Guests/Components/GuestWorkspaceTenant.cs
+++
@@ -1,8 +0,0 @@
-using HotelPms.Shared.MultiTenancy;
-
-namespace HotelPms.Features.Guests.Components;
-
-public static class GuestWorkspaceTenant
-{
-    public static readonly TenantId Id = new(Guid.Parse("f2d66c5a-0f23-44d4-8f7a-63ea5bfe29f0"));
-}
